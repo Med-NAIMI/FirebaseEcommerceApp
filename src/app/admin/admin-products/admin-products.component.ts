2a00fd250b8f380bb288caeed4e1216c176a11bd
MohammedNAIMI <mohammed.naimi@sii-maroc.com>
2022-03-28 13:44:36 +0000

    Searching products  feature

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -6,13 +7,24 @@ import { ProductService } from 'src/app/services/product.service';
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.css']
 })
-export class AdminProductsComponent implements OnInit {
-products$:any;
+export class AdminProductsComponent implements OnInit, OnDestroy {
+products:any[];
+subscription: Subscription;
+filteredProducts: any[];
 
   constructor(private productService: ProductService) {
-    this.products$=productService.getAll();
+    this.subscription= productService.getAll().subscribe(products=>this.filteredProducts=this.products=products);
    }
 
+  filter(query:String){
+    this.filteredProducts=(query) 
+    ? this.products.filter(product=>product.value.title.toLowerCase().includes(query.toLocaleLowerCase())) 
+    : this.products;
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 
   ngOnInit(): void {
   }
